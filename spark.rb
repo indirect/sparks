9709a88e7669669ccb4eb21a5ffb7e7683bef63d
Andre Arko <website+github.com@arko.net>
2011-03-11 13:44:43 -0800

    a tiny campfire library

---
+++ spark.rb
@@ -0,0 +1,119 @@
+require 'uri'
+require 'json'
+require 'net/https'
+
+# spark, a tiny Campfire library
+
+# Usage:
+#   c = Spark::Campfire.new('subdomain', 'abc123')
+#   r = c.room_named "Room Name"
+#   r.say "hi there"
+#   r.paste "class Foo\nend"
+
+# Authors:
+#  Andre Arko <andre@arko.net>
+#  Aaron Patterson <aaron.patterson@gmail.com>
+
+module Spark
+  class Room
+    attr_accessor :id
+
+    def initialize api, name, id
+      @api  = api
+      @name = name
+      @id   = id
+    end
+
+    def method_missing method, *args, &block
+      if @api.respond_to? method
+        args.unshift(@id)
+        @api.send method, *args, &block
+      end
+    end
+  end
+
+  class Campfire
+    attr_reader :uri, :token, :pass
+
+    def initialize subdomain, token
+      @uri   = URI.parse("https://#{subdomain}.campfirenow.com")
+      @token = token
+      @pass  = 'x'
+
+      @http             = Net::HTTP.new(uri.host, uri.port)
+      @http.use_ssl     = true
+      @http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    end
+
+    def room_named name
+      r = rooms.find{|r| r["name"] == name }
+      r ? Room.new(self, name, r["id"]) : nil
+    end
+
+    def rooms
+      @http.start do |http|
+        req = Net::HTTP::Get.new "/rooms.json"
+        req['Content-Type'] = 'application/json'
+        req.basic_auth token, pass
+        begin
+          JSON.parse(http.request(req).body)["rooms"]
+        rescue JSON::ParserError
+          {}
+        end
+      end
+    end
+
+    def post room_id, message, type = nil
+      data = {'body' => message}
+      data.merge!('type' => type) if type
+      json = JSON.generate('message' => data)
+
+      @http.start do |http|
+        req = Net::HTTP::Post.new "/room/#{room_id}/speak.json"
+        req['Content-Type'] = 'application/json'
+        req.basic_auth token, pass
+        http.request(req, json)
+      end
+    end
+
+    def speak room_id, message
+      post room_id, message, 'TextMessage'
+    end
+    alias_method :say, :speak
+
+    def paste room_id, message
+      post room_id, message, 'PasteMessage'
+    end
+
+    def join room_id
+      @http.start do |http|
+        req = Net::HTTP::Post.new "/room/#{room_id}/join.xml"
+        req.basic_auth token, pass
+        http.request(req)
+      end
+    end
+
+    def watch room_id
+      uri = URI.parse('https://streaming.campfirenow.com')
+
+      x             = Net::HTTP.new(uri.host, uri.port)
+      x.use_ssl     = true
+      x.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      x.start do |http|
+        req = Net::HTTP::Get.new "/room/#{room_id}/live.json"
+        req.basic_auth token, pass
+        http.request(req) do |res|
+          res.read_body do |chunk|
+            unless chunk.strip.empty?
+              chunk.split("\r").each do |message|
+                yield JSON.parse(message)
+              end
+            end
+          end
+        end
+      end
+    end
+
+  end
+end
