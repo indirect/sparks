c62e9b981ee1a1ddb2f5a04ccf9290b09c0430a6
Andre Arko <andre@arko.net>
2013-01-12 19:22:48 -0800

    already on a roll, why not try deleting most of it

--- lib/sparks.rb
+++ lib/sparks.rb
@@ -1,6 +1,7 @@
 require 'uri'
 require 'json'
 require 'net/https'
+require 'logger'
 
 # sparks, a tiny Campfire library
 
@@ -11,12 +12,16 @@ require 'net/https'
 #   r.paste "class Foo\nend"
 module Sparks
   class Room
-    attr_accessor :id
+    attr_reader :name, :id
 
     def initialize api, name, id
       @api  = api
       @name = name
       @id   = id
+    end
+
+    def inspect
+      %|#<Sparks::Room:#{object_id} @name=#{name.inspect} @id=#{id.inspect}>|
     end
 
     def method_missing method, *args, &block
@@ -28,13 +33,19 @@ module Sparks
   end
 
   class Campfire
-    attr_reader :uri, :token, :pass
+    attr_reader :logger
 
     def initialize subdomain, token, opts = {}
       @token   = token
       @pass    = 'x'
       @ca_file = opts[:ca_file]
       @http    = http_for("https://#{subdomain}.campfirenow.com")
+      @stream  = http_for("https://streaming.campfirenow.com")
+      @logger  = Logger.new(STDOUT)
+    end
+
+    def me
+      req("/users/me.json")
     end
 
     def room_named(name)
@@ -47,85 +58,40 @@ module Sparks
     end
 
     def room_data
-      @http.start do |http|
-        req = Net::HTTP::Get.new "/rooms.json"
-        req['Content-Type'] = 'application/json'
-        req.basic_auth token, pass
-        begin
-          JSON.parse(http.request(req).body)["rooms"]
-        rescue JSON::ParserError
-          {}
-        end
-      end
+      req("/rooms.json")["rooms"]
     end
 
     def post(room_id, message, type = nil)
       data = {'body' => message}
       data.merge!('type' => type) if type
       json = JSON.generate('message' => data)
-
-      @http.start do |http|
-        req = Net::HTTP::Post.new "/room/#{room_id}/speak.json"
-        req['Content-Type'] = 'application/json'
-        req.basic_auth token, pass
-        http.request(req, json)
-      end
-    end
-
-    def speak(room_id, message)
-      post room_id, message, 'TextMessage'
-    end
-    alias_method :say, :speak
-
-    def paste(room_id, message)
-      post room_id, message, 'PasteMessage'
-    end
-
-    def play(room_id, message)
-      post room_id, message, 'SoundMessage'
-    end
-
-    def tweet(room_id, message)
-      post room_id, message, 'TweetMessage'
-    end
-
-    def join(room_id)
-      @http.start do |http|
-        req = Net::HTTP::Post.new "/room/#{room_id}/join.xml"
-        req.basic_auth token, pass
-        http.request(req)
-      end
-    end
-
-    def watch(room_id)
-      @http.join(room_id)
-      http_for("https://streaming.campfirenow.com").start do |http|
-        req = Net::HTTP::Get.new "/room/#{room_id}/live.json"
-        req.basic_auth token, pass
-        http.request(req) do |res|
-          res.read_body do |chunk|
-            next if chunk.strip.empty?
-            chunk.split("\r").each do |message|
-              yield JSON.parse(message)
-            end
-          end
-        end
-      end
-    rescue => e
-      puts "gotta retry :("
-      raise e
+      req("/room/#{room_id}/speak.json", json)
     end
 
   private
 
     def http_for(url)
       uri = URI.parse(url)
-      Net::HTTP.new(uri.host, uri.port) do |http|
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-        http.ca_file = @ca_file
-        http.ca_file ||= File.expand_path("../rapidssl.crt", __FILE__)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      http.ca_file = @ca_file
+      http.ca_file ||= File.expand_path("../rapidssl.crt", __FILE__)
+      http
+    end
+
+    def req(path, json = nil)
+      res = @http.start do |http|
+        verb = json ? Net::HTTP::Post : Net::HTTP::Get
+        req = verb.new path
+        req['Content-Type'] = 'application/json'
+        req.basic_auth @token, @pass
+        res = json ? http.request(req, json) : http.request(req)
       end
+      JSON.parse(res.body)
+    rescue JSON::ParserError
+      logger.info "Couldn't parse response: #{res.body}"
+      {}
     end
 
   end
